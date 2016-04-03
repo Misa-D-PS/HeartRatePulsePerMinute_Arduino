347e732b25b9f77eee71aa07c784c8df3061ae57
Phongsit Misa <ps.d.misa@gmail.com>
2016-04-03 17:46:10 +0700

    Added files via upload

---
+++ PulsePerMinute_Arduino.ino
@@ -0,0 +1,24 @@
+unsigned int pulseCount;
+unsigned int sensor;
+unsigned int pulsePerMinute;
+unsigned int threshold = 100;
+
+void setup() {
+  pinMode(A0,INPUT);
+  Serial.begin(57600);
+}
+
+void loop() {
+  sensor = analogRead(A0);
+    while((millis()%1000) != 0){
+      if(sensor>treshold){
+        pulseCount++;
+      }
+    }
+
+    pulsePerMinute = pulseCount*60;
+    Serial.print("Pulse per minute = ");
+    Serial.println(pulsePerMinute);
+    pulseCount = 0;
+
+}
