593a7142ce4d99f3e101b13aca76c7d360398e11
Phongsit Misa <ps.d.misa@gmail.com>
2016-04-03 18:58:56 +0700

    Update PulsePerMinute_Arduino.ino

--- PulsePerMinute_Arduino.ino
+++ PulsePerMinute_Arduino.ino
@@ -2,9 +2,13 @@ unsigned int pulseCount;
 unsigned int sensor;
 unsigned int pulsePerMinute;
 unsigned int threshold = 100;
+unsigned int setpoint = 100;
+
+int LED = 13;
 
 void setup() {
   pinMode(A0,INPUT);
+  pinMode(LED,OUTPUT);
   Serial.begin(57600);
 }
 
@@ -17,6 +21,12 @@ void loop() {
     }
 
     pulsePerMinute = pulseCount*60;
+    if(pulsePerMinute<setpoint){
+        digitalWrite(LED,HIGH);
+    }
+    else{
+        digitalWrite(LED,LOW);
+    }
     Serial.print("Pulse per minute = ");
     Serial.println(pulsePerMinute);
     pulseCount = 0;
